47e06749705bbaa865fa358b2237b5149678e8df
jo-aguilar <joaoaguilar93@gmail.com>
2021-04-16 10:48:32 -0300

    Livrarias majoritariamente funcionando

--- GerenciadorBiblioteca.java
+++ GerenciadorBiblioteca.java
@@ -17,7 +17,13 @@ Atlas: retirada da biblioteca proibida
 //============================================================================//
 //                                 SUPERCLASSES                               //
 //============================================================================//
+import java.util.Random;
+import java.util.ArrayList;
+
 class Individuo{
+  /*Objeto básico para a criação de objetos relacionados aos indivíduos com algum
+  vínculo com a biblioteca, podendo ser de usuários registrados a funcionários do
+  local*/ 
   protected String nome;
   protected String status;   // status/cargo do indivíduo mediante a instituição
   protected boolean permissao_retirada;
@@ -31,7 +37,11 @@ class Individuo{
   boolean retornaPermissao(){return permissao_retirada;}
 }
 
+
+
 class Brochura{
+   /*Objeto básico para a criação de objetos relacionados aos livros e análogos ca-
+   talogados na biblioteca, podendo ser de revistas e livros e atlas para consulta */
    protected String nome;
    protected String autor;
    protected String ISBN;
@@ -40,15 +50,28 @@ class Brochura{
    public Brochura(String nn, String aut, int qtd){
       nome = nn;
       autor = aut;
-      //ISBN = gera_isbn();
       quantidade_in_situ = qtd;
       quantidade_disponivel = qtd;
+      ISBN = geraISBN().toString();
    }
    public String retornaNome(){return nome;}
    public String retornaAutor(){return autor;}
    public String retornaISBN(){return ISBN;}
    public int retornaInSitu(){return quantidade_in_situ;}
    public int retornaDisponivel(){return quantidade_disponivel;}
+   private StringBuilder geraISBN(){
+   /*Gera um registro aleatório de ISBN para que uma brochura qualquer possa
+   ser catalogada no sistema  de livros*/
+      StringBuilder resultado = new StringBuilder();
+      Random rand = new Random();
+      for(int clk = 0; clk < 13; clk++){
+         if(clk==4 || clk==6 || clk==11)
+            resultado.append("-");
+         else
+            resultado.append(Integer.toString(rand.nextInt(9)));
+      }
+      return resultado;
+   }
 }
 
 //============================================================================//
@@ -57,35 +80,83 @@ class Brochura{
 
 class Usuario extends Individuo{
    private float valor_multa;
-   public Usuario(String nn, String ss, boolean pr){
-       super(nn, ss, pr);
+   private int dias_excedentes;
+   public Usuario(String nn){
+       super(nn, "usuário", true);
+       valor_multa = 5;
    }
    public void setaPermissao(boolean pp){
      //modifica a permissão do Usuário para retirada ou não de livros, pois,
      //diferente de funcionários, eles não têm acesso irrestrito aos livros
      permissao_retirada = pp;
    }
-   public void setaMulta(tempo){
-      //fórmula para computação da multa;
+   public float setaMulta(int tempo){
+      //multa aplicada em 10% cumulativos do valor original sobre a multa
+      //original;
+      float multa_aux = valor_multa;
+      for(int clk = 0; clk < tempo; clk++){
+         multa_aux *= 1.1;
+      }
+      return multa_aux;
    }
-   public float retornaMulta(){return valor_multa;}
-}
+   public void setaExcedente(int dias){dias_excedentes = dias;}
+   public float retornaMulta(){return setaMulta(dias_excedentes);}
 
+}
 
 class Funcionario extends Individuo{
-   public Funcionario(String nn, String ss){
+   public Funcionario(String nn){
    //terceiro argumento de super em true implia em funcionários terem acesso
    //irrestrito aos livros da biblioteca, sem necessidade de manutenção
-   super(nn, ss, true);
+   super(nn, "funcionário", true);
    }
 }
 
 
+
+public class GerenciadorBiblioteca{
+   //fazer função para a devolução e reincremento de título
+   
+   /*
+   Individuo     : funcionando
+   Usuario       : funcionando
+   Funcionario   : funcionando
+   */
 
 //============================================================================//
 //                                     MAIN                                   //
 //============================================================================//
-public class GerenciadorBiblioteca{
-  static public void main(String[]args){
-  }
+   static public void main(String[]args){
+      Brochura livro = new Brochura("A Tempestade", "Shakespeare", 20);
+      mostra_brochura(livro);
+      Individuo usuario = new Individuo("Joacir França", "Usuário", true);
+      mostra_individuo(usuario);
+      Funcionario func = new Funcionario("Antonio Neves");
+      mostra_individuo(func);
+      Usuario us = new Usuario("João Dias");
+      mostra_individuo(us);
+      System.out.println(us.setaMulta(5));
+   }
+//============================================================================//
+//                                 FIM DE MAIN                                //
+//============================================================================//
+
+
+
+   //teste para verificar a integridade do objeto Brochura
+   static public void mostra_brochura(Brochura bro){
+      System.out.println("Nome do livro        : " + bro.retornaNome());
+      System.out.println("Nome do autor        : " + bro.retornaAutor());
+      System.out.println("ISBN                 : " + bro.retornaISBN());
+      System.out.println("Quantidade disponível: " + bro.retornaDisponivel());
+      System.out.println("Quantidade in situ   : " + bro.retornaInSitu() + "\n");
+   }
+
+   //teste para verificar a integridade do objeto Individuo
+   static public void mostra_individuo(Individuo ind){
+      System.out.println("Nome do individuo     : " + ind.retornaNome());
+      System.out.println("Status do individuo   : " + ind.retornaStatus());
+      System.out.println("Permissão do indivíduo: " + ind.retornaPermissao() + "\n");
+   }
+
 }
